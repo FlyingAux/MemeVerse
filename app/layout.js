bbfe018ec875b94105478c54d62e066483220b8c
rajdev04 <rajdevshrivastava72@gmail.com>
2025-02-27 04:17:34 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Nav/>
+        <Nav></Nav>
         {children}
       </body>
     </html>
